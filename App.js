6652cd8cb00fae7fb5e629aa5fdd7f964998e441
Abdul Ahad <abdulahad.momin07@gmail.com>
2023-06-18 19:13:25 +0530

    feat(carousel): added introduction sliders using carousel

--- App.js
+++ App.js
@@ -6,117 +6,131 @@
  * @flow strict-local
  */
 
-import React, {useEffect} from 'react';
-import {Platform} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
+  Button,
+  Image,
+  Platform,
+  TouchableWithoutFeedback,
   View,
 } from 'react-native';
+import {Text} from 'react-native';
+import {Dimensions} from 'react-native';
+import {SafeAreaView} from 'react-native';
+import Carousel, {Pagination} from 'react-native-snap-carousel';
 import SplashScreen from 'react-native-splash-screen';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import IMAGES from './src/assets/images';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
   useEffect(() => {
     if (Platform.OS === 'android') SplashScreen.hide();
   }, []);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const _carousel = useRef();
+
+  const [activeDotIndex, setActiveDotIndex] = useState(0);
+
+  const data = [
+    {
+      id: 1,
+      title: 'Browse Product',
+      image: IMAGES.BROWSE,
+      description:
+        'Browse by category or brand from a variety of more than 1000 products',
+    },
+    {
+      id: 2,
+      title: 'Pay Seamlessly',
+      image: IMAGES.PAY,
+      description:
+        'Pay using your preferred method including online or card payment',
+    },
+    {
+      id: 2,
+      title: 'Track Order',
+      image: IMAGES.TRACK,
+      description: 'Track your order from the warehouse to your doorstep',
+    },
+  ];
+
+  const _renderItem = ({item, index}) => {
+    return (
+      <View>
+        <Image
+          source={item.image}
+          style={{
+            height: Dimensions.get('window').width,
+            width: Dimensions.get('window').width,
+          }}
+        />
+        <View style={{padding: 15}}>
+          <Text style={{fontSize: 18, fontWeight: '700'}}>{item.title}</Text>
+          <Text style={{marginTop: 20, fontSize: 16}}>{item.description}</Text>
+        </View>
+      </View>
+    );
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <SafeAreaView style={{flex: 1, justifyContent: 'space-between'}}>
+      <View style={{alignItems: 'flex-end', padding: 15}}>
+        <Button title="Skip" />
+      </View>
+      <Carousel
+        ref={_carousel}
+        data={data}
+        renderItem={_renderItem}
+        sliderWidth={Dimensions.get('window').width}
+        itemWidth={Dimensions.get('window').width}
+        onSnapToItem={index => setActiveDotIndex(index)}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <Pagination
+          carouselRef={_carousel}
+          activeDotIndex={activeDotIndex}
+          dotsLength={3}
+          dotStyle={{
+            width: 15,
+            backgroundColor: 'orange',
+          }}
+          inactiveDotStyle={{
+            width: 10,
+            height: 10,
+            backgroundColor: 'gray',
+          }}
+        />
+        <View style={{padding: 15, flexDirection: 'row'}}>
+          <TouchableWithoutFeedback
+            onPress={() => {
+              _carousel.current.snapToItem(activeDotIndex - 1);
+            }}>
+            <View
+              style={{
+                height: 50,
+                width: 50,
+                borderRadius: 25,
+                backgroundColor: 'lightgray',
+                marginEnd: 10,
+              }}
+            />
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback
+            onPress={() => {
+              _carousel.current.snapToItem(activeDotIndex + 1);
+            }}>
+            <View
+              style={{
+                height: 50,
+                width: 50,
+                borderRadius: 25,
+                backgroundColor: 'orange',
+              }}
+            />
+          </TouchableWithoutFeedback>
         </View>
-      </ScrollView>
+      </View>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
